3ded9d8aa8450cc7174b1f1eb6dfae14fef055c1
Semyon <inary.wrk@gmail.com>
2021-05-05 19:34:08 +0300

    wip

---
+++ MetricsManager/MetricsAgent/DIContainer.cs
@@ -0,0 +1,14 @@
+﻿using StrongInject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent
+{
+    [Register(typeof(Controllers.CPUController), Scope.InstancePerResolution)]
+    public partial class DIContainer: IContainer<Controllers.CPUController>
+    {
+    }
+}
--- MetricsManager/MetricsAgent/Startup.cs
+++ MetricsManager/MetricsAgent/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StrongInject.Extensions.DependencyInjection;
 
 namespace MetricsAgent
 {
@@ -26,9 +27,9 @@ namespace MetricsAgent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().ResolveControllersThroughServiceProvider();
+            services.AddTransientServiceUsingContainer<DIContainer, Controllers.CPUController>();
 
-            services.AddControllers();
-            services.AddTransient<IDateTimeProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
